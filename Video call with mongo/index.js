e3b4e0db9c32eb48edd33aa59d05a387f7a0a68a
zc-razanaqvi <118174821+zc-razanaqvi@users.noreply.github.com>
2023-11-17 13:38:29 +0500

    rtcpeerconnection

--- Video call with mongo/index.js
+++ Video call with mongo/index.js
@@ -37,11 +37,25 @@ var wss = new webSocketServ({
     port: 8000
 });
 
+var users = {};
+
 wss.on("connection", function (conn) {
     console.log("User connected");
 
-    conn.on("message", function () {
-
+    conn.on("message", function (message) {
+        var data;
+        try {
+            data = JSON.parse(message);
+        } catch (error) {
+            console.log("message error", error);
+        };
+        switch (data.type) {
+            case "online":
+                users[data.name] = conn;
+                conn.name = data.name;
+                sendToOtherUser(conn, { type: "online", success: true });
+                break;
+        };
     });
 
     conn.on("close", function () {
--- Video call with mongo/public/js/client.js
+++ Video call with mongo/public/js/client.js
@@ -1,7 +1,21 @@
 var connection = new WebSocket("ws://localhost:8000");
 var local_video = document.querySelector('#local-video');
+var call_to_username_input = document.querySelector('#username-input');
+var call_btn = document.querySelector('#call-btn');
 var name;
 var connectedUser;
+var myConn;
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+call_btn.addEventListener("click", function () {
+    var call_to_username = call_to_username_input.value;
+    if (call_to_username.length > 0) {
+        connectedUser = call_to_username;
+    } else {
+        alert("Enter a name to call");
+    };
+});
 
 connection.onopen = function () {
     console.log("Connected to server");
@@ -9,6 +23,11 @@ connection.onopen = function () {
 
 connection.onmessage = function (msg) {
     var data = JSON.parse(msg.data);
+    switch (data.type) {
+        case "online":
+            onlineProcess(data.success);
+            break;
+    };
 };
 
 connection.onerror = function (err) {
@@ -34,18 +53,33 @@ function send(message) {
     connection.send(JSON.stringify(message));
 };
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-navigator.getUserMedia(
-    {
-        audio: true,
-        video: true
-    },
-    function (myStream) {
-        stream = myStream;
-        local_video.srcObject = stream;
-    },
-    function (err) {
-        alert("something went wrong", err);
-    },
-);
+function onlineProcess(success) {
+    if (success) {
+        navigator.getUserMedia(
+            {
+                audio: true,
+                video: true
+            },
+            function (myStream) {
+                stream = myStream;
+                local_video.srcObject = stream;
+                var configuration = {
+                    "iceServers": [
+                        { "urls": "stun:stun2.1.google.com:19302" }
+                    ],
+                };
+                myConn = new webkitRTCPeerConnection(configuration, {
+                    optional: [{
+                        RtpDataChannels: true,
+                    }],
+                });
+                myConn.addStream(stream);
+            },
+            function (err) {
+                alert("something went wrong", err);
+            },
+        );
+    } else {
+        alert("something went wrong");
+    };
+};
